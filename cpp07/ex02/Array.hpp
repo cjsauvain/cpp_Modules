621e3992d5811f754cf96b21319ee3530f4895b4
Jordan Sauvain <jsauvain@3E1.42angouleme.fr>
2023-04-17 16:45:41 +0200

    C++ templates

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,55 +6,86 @@
 /*   By: jsauvain <jsauvain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 16:12:11 by jsauvain          #+#    #+#             */
-/*   Updated: 2023/04/16 16:31:47 by jsauvain         ###   ########.fr       */
+/*   Updated: 2023/04/17 16:10:27 by jsauvain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
 
 template <typename T>
 class Array
 {
 	public:
-		Array<T>(void);
-		Array<T>(unsigned int n);
-		Array<T>(Array<T> const & source);
-		~Array<T>(void);
+		Array(void);
+		Array(unsigned int n);
+		Array(Array<T> const & source);
+		~Array(void);
 
-		Array<T> & const	operator=(Array<T> const & source);
+		T &			operator[](long long int i);
+		Array &	operator=(Array<T> const & source);
 
-		int	size() const;
+		unsigned int	size() const;
 
 	private:
-		T *	_array;
+		T *				_array;
+		unsigned int	_arrayLength;
 };
 
 template <typename T>
-Array<T>::Array<T>(void)
+Array<T>::Array(void) : _arrayLength(0)
 {
-	_array = new T(1);
-	_array[0] = 0;
+	_array = NULL;
 }
 
 template <typename T>
-Array<T>::Array<T>(unsigned int n)
+Array<T>::Array(unsigned int n) : _arrayLength(n)
 {
-	_array = new T(n);
+	_array = new T[n]();
 }
 
 template <typename T>
-Array<T>::Array<T>(Array<T> const & source)
+Array<T>::Array(Array<T> const & source)
 {
 	*this = source;
 }
 
 template <typename T>
-Array<T> & const	operator=(Array<T> const & source)
+Array<T>::~Array(void)
+{
+	if (_array)
+		delete []_array;
+}
+
+template <typename T>
+Array<T> &	Array<T>::operator=(Array<T> const & source)
 {
 	if (this != &source)
 	{
+		_arrayLength = source._arrayLength;
 		delete _array;
-		_array = new T(source.size());
-		for (int i = 0; i < 
+		_array = new T(_arrayLength);
+		for (unsigned int i = 0; i < _arrayLength; i++)
+			_array[i] = source._array[i];
 	}
+	return *this;
+}
+
+template <typename T>
+T &	Array<T>::operator[](long long int i)
+{
+	if (i > _arrayLength)
+		throw std::out_of_range("std::exception : index out");
+	if (i < 0 || i > _arrayLength)
+		throw std::out_of_range("std::exception : index out of range\n");
+	if (_arrayLength == 0)
+		throw std::out_of_range("std::exception : empty array");
+	return (_array[i]);
+}
+
+template <typename T>
+unsigned int	Array<T>::size(void) const
+{
+	return (_arrayLength);
 }
---
+++ cpp07/ex02/main.cpp
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsauvain <jsauvain@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 14:48:04 by jsauvain          #+#    #+#             */
+/*   Updated: 2023/04/17 16:18:10 by jsauvain         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Array.hpp"
+
+int	main(void)
+{
+	std::cout << "------------ Test empty array ------------\n\n";
+	{
+		Array<int>	tmp;
+
+		try{
+			std::cout << tmp[0] << std::endl;
+		}
+		catch (std::exception & e){
+			std::cout << e.what() << std::endl;
+		}
+		std::cout << std::endl;
+	}
+	std::cout << "------------ Test int array --------------\n\n";
+	{
+		Array<int>	tmp(5);
+		
+		srand(time(NULL));
+		for (unsigned int i = 0; i < tmp.size(); i++)
+		{
+			tmp[i] = std::rand() % 10;
+			std::cout << "index " << i << " : " << tmp[i] << std::endl;
+		}
+		std::cout << std::endl;
+		try{
+			std::cout << tmp[-1] << std::endl;
+		}
+		catch (std::exception & e){
+			std::cout << e.what();
+		}
+		try{
+			std::cout << tmp[8] << std::endl;
+		}
+		catch (std::exception & e){
+			std::cout << e.what();
+		}
+		std::cout << std::endl << std::endl;
+	}
+	std::cout << "------------ Test char array -------------\n\n";
+	{
+		Array<char>	tmp(5);
+		
+		srand(time(NULL));
+		for (unsigned int i = 0; i < tmp.size(); i++)
+		{
+			tmp[i] = std::rand() % 94 + 32;
+			std::cout << "index " << i << " : " << tmp[i] << std::endl;
+		}
+		std::cout << std::endl;
+		try{
+			std::cout << tmp[-1] << std::endl;
+		}
+		catch (std::exception & e){
+			std::cout << e.what();
+		}
+		try{
+			std::cout << tmp[8] << std::endl;
+		}
+		catch (std::exception & e){
+			std::cout << e.what();
+		}
+		std::cout << std::endl << std::endl;
+	}
+	std::cout << "------------ Test char const * array -------------\n\n";
+	{
+		Array<char const *>	tmp(5);
+		
+		tmp[0] = "oui";
+		tmp[1] = "non";
+		tmp[2] = "toto";
+		tmp[3] = "tata";
+		tmp[4] = "lala";
+		for (unsigned int i = 0; i < tmp.size(); i++)
+			std::cout << "index " << i << " : " << tmp[i] << std::endl;
+		std::cout << std::endl;
+		try{
+			std::cout << tmp[-1] << std::endl;
+		}
+		catch (std::exception & e){
+			std::cout << e.what();
+		}
+		try{
+			std::cout << tmp[8] << std::endl;
+		}
+		catch (std::exception & e){
+			std::cout << e.what();
+		}
+		std::cout << std::endl << std::endl;
+	}
+	std::cout << "------------------------------------------\n\n";
+}
